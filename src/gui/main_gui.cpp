7836d27fe902fd2166a0b5970a96e4842613d735
Mads Sejerskilde <madssejerskilde2@gmail.com>
2025-12-06 22:07:27 +0100

    first commit

---
+++ src/gui/main_gui.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+
+
+int main(){
+    //create the main window
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+
+    while (true){}
+
+
+
+}
+
+
